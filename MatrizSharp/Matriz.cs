29bbc573095744f14ce807487f9ec2385fdbf5e6
Adroaldo <hygino@msn.com>
2022-01-06 17:08:15 -0300

    Escrita da matriz

---
+++ MatrizSharp/Matriz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrizSharp
+{
+    class Matriz
+    {
+        public static void EscrevaMatriz(double[,] matriz)
+        {
+            int linha = matriz.GetLength(0);
+            int coluna = matriz.GetLength(1);
+            Console.WriteLine("A matriz possui dimensões" + linha + " x " + coluna);
+        }
+    }
+}
---
+++ MatrizSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MatrizSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            double[,] m = new double[,] { { 3, 1 }, { 8, 7 }, { 9, 5 } };
+
+            Matriz.EscrevaMatriz(m);
+        }
+    }
+}
